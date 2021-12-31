67db3f27a0f9b64211b0c8a6eb96e3f2d73f57d7
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-12-31 09:56:47 +0900

    Add `get_cards` method

--- trelloclient.py
+++ trelloclient.py
@@ -17,3 +17,8 @@ class TrelloClient:
         api_url = f"{self._trello_api_url}/boards/{board_id}/lists"
         response = requests.get(f"{api_url}{self.auth_query_string}")
         return [CardList(data) for data in response.json()]
+
+    def get_cards(self, card_list_id: str) -> list:
+        api_url = f"{self._trello_api_url}/lists/{card_list_id}/cards"
+        response = requests.get(f"{api_url}{self.auth_query_string}")
+        return response.json()
