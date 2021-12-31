a78819f16af9348f3f68f9145c4187b6e1d0066b
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-31 15:58:52 +0900

    Replace two leading spaces with one asterisk

--- test_utils.py
+++ test_utils.py
@@ -3,7 +3,6 @@ import utils
 
 def test_replace_leading_spaces_with_asterisks():
     assert utils.replace_leading_spaces_with_asterisks("str") == "str"
-    assert utils.replace_leading_spaces_with_asterisks(" str") == "*str"
-    assert utils.replace_leading_spaces_with_asterisks("  str") == "**str"
-    assert utils.replace_leading_spaces_with_asterisks("   str") == "***str"
+    assert utils.replace_leading_spaces_with_asterisks("  str") == "*str"
+    assert utils.replace_leading_spaces_with_asterisks("    str") == "**str"
 
