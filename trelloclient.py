1fe3c9185fbcdefdf554887f700519f6f8446b31
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-31 13:35:37 +0900

    Extract `get_response_data` method

--- trelloclient.py
+++ trelloclient.py
@@ -14,17 +14,20 @@ class TrelloClient:
     def auth_query_string(self):
         return f"?key={self._key}&token={self._token}"
 
+    def get_response_data(self, api_url) -> dict:
+        response = requests.get(f"{api_url}{self.auth_query_string}")
+        return response.json()
+
     def get_card_lists(self, board_id: str) -> list[CardList]:
         api_url = f"{self._trello_api_url}/boards/{board_id}/lists"
-        response = requests.get(f"{api_url}{self.auth_query_string}")
-        return [CardList(data) for data in response.json()]
+        return [CardList(data) for data in self.get_response_data(api_url)]
 
     def get_cards(self, card_list_id: str) -> list:
         api_url = f"{self._trello_api_url}/lists/{card_list_id}/cards"
-        response = requests.get(f"{api_url}{self.auth_query_string}")
-        return [Card(card_data) for card_data in response.json()]
+        return [
+            Card(card_data) for card_data in self.get_response_data(api_url)
+        ]
 
     def get_member_name(self, member_id: str) -> str:
         api_url = f"{self._trello_api_url}/members/{member_id}"
-        response = requests.get(f"{api_url}{self.auth_query_string}")
-        return response.json().get("fullName")
+        return self.get_response_data(api_url).get("fullName")
