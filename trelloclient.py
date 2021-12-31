7d421b0987da8eb0d5ed517cffb06951de9626b7
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-12-31 09:20:04 +0900

    Change the declaration of the contructor

--- trelloclient.py
+++ trelloclient.py
@@ -1,5 +1,5 @@
 class TrelloClient:
-    def __init__(self, auth_key, auth_token) -> None:
-        self._key = auth_key
-        self._token = auth_token
+    def __init__(self, auth: dict) -> None:
+        self._key = auth.get("key")
+        self._token = auth.get("token")
         self._trello_api_url = "https://api.trello.com/1"
