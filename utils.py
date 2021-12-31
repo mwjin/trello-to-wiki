f48f8c2e0288ab9a28dac11ef5679e8811684218
Minwoo Jeong <jeongmwj@gmail.com>
2021-12-31 15:47:05 +0900

    Implement a logic to replace leading spalces with asterisks

---
+++ utils.py
@@ -0,0 +1,4 @@
+def replace_leading_spaces_with_asterisks(string):
+    stripped_str = string.lstrip()
+    leading_space_cnt = len(string) - len(stripped_str)
+    return f"{'*' * leading_space_cnt}{stripped_str}"
