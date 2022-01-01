143b0b7f08b11df0deda5d43e9880f117403db22
Minwoo Jeong <jeongmwj@gmail.com>
2022-01-01 22:13:45 +0900

    Print logs

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+import sys
+from collections import defaultdict
+from datetime import datetime
+
 import yaml
 
 from trelloclient import TrelloClient
-from collections import defaultdict
 
 
 def main():
     with open("auth.yml") as infile:
         auth = yaml.safe_load(infile)
     client = TrelloClient(auth)
+
+    print_log("Collect the card lists from the Trello board.")
     board_id = "21H6HzC7"
     card_lists = client.get_card_lists(board_id)
-    
+
     card_map = defaultdict(list)
     for card_list in card_lists:
+        print_log(f"Collect the cards from the list '{card_list.name}'.")
         cards = client.get_cards(card_list.id)
         for card in cards:
             card.list_name = card_list.name
@@ -20,10 +26,21 @@ def main():
                 card.add_member_name(client.get_member_name(member_id))
             card_map[card.category].append(card)
 
+    print_log(f"Print the wiki.")
     for category in card_map:
         print(f"=={category}==")
         for card in sorted(card_map[category], key=lambda x: x.name):
             print(card.wiki)
+
+
+def print_log(msg: str):
+    print(f"[{_get_curr_time()}] {msg}", file=sys.stderr)
+
+
+def _get_curr_time() -> str:
+    now = datetime.now()
+    curr_time = now.strftime("%H:%M:%S %m/%d/%y")
+    return curr_time
 
 
 if __name__ == "__main__":
